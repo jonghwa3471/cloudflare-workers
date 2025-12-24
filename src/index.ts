4d17f50131b4788ca746bcc31da80b7eae81c4b1
Jonghwa <whdghk3471@gmail.com>
2025-12-24 21:45:05 +0900

    #20.2 Routing

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,18 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Bind resources to your worker in `wrangler.jsonc`. After adding bindings, a type definition for the
- * `Env` object can be regenerated with `npm run cf-typegen`.
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+// @ts-ignore
+import home from './home.html';
 
 export default {
 	async fetch(request, env, ctx): Promise<Response> {
-		return new Response('Hello World!');
+		const url = new URL(request.url);
+		if (url.pathname === '/') {
+			return new Response(home, {
+				headers: {
+					'Content-Type': 'text/html;charset=utf-8',
+				},
+			});
+		}
+		return new Response(null, {
+			status: 404,
+		});
 	},
 } satisfies ExportedHandler<Env>;

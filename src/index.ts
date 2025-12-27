7d2dc48388b9c0e145a0999989970e2d1bf9b550
Jonghwa <whdghk3471@gmail.com>
2025-12-27 16:19:52 +0900

    #20.4 Visit Counter

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,56 @@
 // @ts-ignore
 import home from './home.html';
 
+function handleHome() {
+	return new Response(home, {
+		headers: {
+			'Content-Type': 'text/html;charset=utf-8',
+		},
+	});
+}
+
+function handleNotFound() {
+	return new Response(null, {
+		status: 404,
+	});
+}
+
+function handleBadRequest() {
+	return new Response(null, {
+		status: 400,
+	});
+}
+
+async function handleVisit(searchParams: URLSearchParams, env: Env) {
+	const page = searchParams.get('page');
+	if (!page) {
+		return handleBadRequest();
+	}
+	const kvPage = await env.view_counter.get(page);
+	let value = 1;
+	if (!kvPage) {
+		await env.view_counter.put(page, String(value));
+	} else {
+		value = Number(kvPage) + 1;
+		await env.view_counter.put(page, String(value));
+	}
+	return new Response(
+		JSON.stringify({
+			visits: value,
+		})
+	);
+}
+
 export default {
 	async fetch(request, env, ctx): Promise<Response> {
-		const url = new URL(request.url);
-		if (url.pathname === '/') {
-			await env.view_counter.put('hello', 'how are you?');
-			return new Response(home, {
-				headers: {
-					'Content-Type': 'text/html;charset=utf-8',
-				},
-			});
+		const { pathname, searchParams } = new URL(request.url);
+		switch (pathname) {
+			case '/':
+				return handleHome();
+			case '/visit':
+				return handleVisit(searchParams, env);
+			default:
+				return handleNotFound();
 		}
-		return new Response(null, {
-			status: 404,
-		});
 	},
 } satisfies ExportedHandler<Env>;

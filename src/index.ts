9604e4d388c78e022a47d7ba0691057b2e57ea4a
Jonghwa <whdghk3471@gmail.com>
2025-12-27 15:16:55 +0900

    #20.3 Workers KV

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,7 @@ export default {
 	async fetch(request, env, ctx): Promise<Response> {
 		const url = new URL(request.url);
 		if (url.pathname === '/') {
+			await env.view_counter.put('hello', 'how are you?');
 			return new Response(home, {
 				headers: {
 					'Content-Type': 'text/html;charset=utf-8',

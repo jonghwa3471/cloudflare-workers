cb4d846585b65c461587c6fd4a54f2d261130373
Jonghwa <whdghk3471@gmail.com>
2025-12-28 17:58:20 +0900

    #20.6 Durable Objects

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,5 @@
 // @ts-ignore
 import home from './home.html';
-import { makeBadge } from './utils';
 
 function handleHome() {
 	return new Response(home, {
@@ -16,40 +15,12 @@ function handleNotFound() {
 	});
 }
 
-function handleBadRequest() {
-	return new Response(null, {
-		status: 400,
-	});
-}
-
-async function handleVisit(searchParams: URLSearchParams, env: Env) {
-	const page = searchParams.get('page');
-	if (!page) {
-		return handleBadRequest();
-	}
-	const kvPage = await env.view_counter.get(page);
-	let value = 1;
-	if (!kvPage) {
-		await env.view_counter.put(page, String(value));
-	} else {
-		value = Number(kvPage) + 1;
-		await env.view_counter.put(page, String(value));
-	}
-	return new Response(makeBadge(value), {
-		headers: {
-			'Content-Type': 'image/svg+xml;charset=utf-8',
-		},
-	});
-}
-
 export default {
-	async fetch(request, env, ctx): Promise<Response> {
+	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
 		const { pathname, searchParams } = new URL(request.url);
 		switch (pathname) {
 			case '/':
 				return handleHome();
-			case '/visit':
-				return handleVisit(searchParams, env);
 			default:
 				return handleNotFound();
 		}
